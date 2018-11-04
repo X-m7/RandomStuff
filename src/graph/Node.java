9c16f9bde585d4bd99f582e5a46f82fbd1491746
Prajna Sariputra <putr4.s@gmail.com>
2018-11-04 15:49:24 +1100

    Node to act as part of an adjacency list

---
+++ src/graph/Node.java
@@ -0,0 +1,28 @@
+package graph;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Node<T> {
+
+	private T value;
+	private List<Node<T>> adjacentNodes;
+	
+	public Node(T newValue) {
+		value = newValue;
+		adjacentNodes = new LinkedList<Node<T>>();
+	}
+
+	public T getValue() {
+		return value;
+	}
+
+	public List<Node<T>> getAdjacentNodes() {
+		return Collections.unmodifiableList(adjacentNodes);
+	}
+	
+	public void addAdjacentNode(Node<T> node) {
+		adjacentNodes.add(node);
+	}
+}
